0ffb6d7913cf5540708d07626afe54b3c023b80f
Joshua Ji <joshua.ji@jiwhiz.com>
2024-11-10 06:58:49 +0000

    update test

--- test/javascript-node-edgedb/test.sh
+++ test/javascript-node-edgedb/test.sh
@@ -3,6 +3,7 @@ cd $(dirname "$0")
 source test-utils.sh
 
 # Template specific tests
+edgedb instance link edgedb-docker --trust-tls-cert --host=edgedb --port=5656 --password
 
 # Report result
 reportResults
